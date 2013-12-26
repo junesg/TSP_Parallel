49c004edd6cce31d10e456b4aaccfb2876d936d4
Yuan Shangguan <juneysg@umich.edu>
2013-12-26 17:19:04 -0500

    finished graph DFS algorithm

--- graph.cpp
+++ graph.cpp
@@ -1,53 +1,79 @@
 #include "graph.h"
+#include <iostream>
+#include <ctime>
+using namespace std;
 
-//constructor for graph
-Graph::Graph(std::vector< pair<int, int> > initialsolution, std::vector<std::pair<int,int> > coordinates1, string filename){
-    init_solution = initialsolution;
-    coordinates = coordinates1;
-    name = filename;
+
+//After constructor for the graph, we now have create graph, which puts in necessary parts
+void Graph::createGraph(vector< pair<int, int> > initialsolution, vector<pair<int,int> > coordinates){
     
-    //create vertices of the graph
-    std::vector< pair<int, int> > ::iterator it;
-    for (it=init_solution.begin(); it!=init_solution.end(); it++) {
-        std::set<Vertices>::iterator viter;
-        int found =0;
-        for (viter = allVert.begin(); viter!= allVert.end(); viter++) {
-            if (it->first == viter->vertInd) {
-                //this vertex already exists
-                (*it).addNeighbor(it->second);
-                found = found + 1 ; //found ==1
-            }
-            if (it->second == viter->vertInd) {
-                //this vertex already exists
-                (*it).addNeighbor(it->first);
-                found = found  + 2; //found ==2 or 3
-            }
-            if (found == 3) {
-                break;
-            }
-        }
-        
-        switch (found) {
-            case 0://neither is found
-                //create vertex 1 and 2, add to allVert
-                
-                Vertices vec1 = new Vertices();
-                break;
-                
-            case 1://only first one found
-                
-                break;
-            case 2://only second one is found
-                
-                break;
-                
-            case 3://both found
-                
-            default:
-                break;
-        }
+    //construct edges vector
+    for (vector<pair<int,int> >::iterator it = initialsolution.begin(); it!= initialsolution.end(); it++) {
+        Edges* newEdge = new Edges;
+        newEdge->fromIndex = it->first;
+        newEdge->toIndex = it->second;
+        newEdge->revised = false;
+        allEdge.push_back(newEdge);
+    }
+    //construct vertex vector
+    int count = 0;
+    for (vector<pair<int,int> >::iterator it = coordinates.begin(); it!= coordinates.end(); it++) {
+        Vertices* newVert = new Vertices;
+        newVert->index = count;
+        newVert->xPos = it->first;
+        newVert->yPos = it->second;
+        newVert->visited = false;
+        allVert.push_back(newVert);
+        count ++;
+    }
+    //Now find the neighbors of the current edges
+    for (vector<pair<int,int> >::iterator it = initialsolution.begin();it!= initialsolution.end(); it++) {
+        Vertices *p1,*p2;
+        p1 = findVertex(it->first);
+        p2 = findVertex(it->second);
+        p1 -> neighbors.push_back(p2->index);
+        p2 -> neighbors.push_back(p1->index);
+    }
+}
 
-        
+//Destructor for the graph
+Graph::~Graph(){
+    //delete all vertices
+    for (vector<Vertices*>::iterator it = allVert.begin(); it!=allVert.end(); it++) {
+        delete (*it);
+    }
+    //delete all edges
+    for (vector<Edges*>::iterator it=allEdge.begin(); it!=allEdge.end(); it++) {
+        delete (*it);
+    }
+}
+
+//given an index to the vertex, find the pointer to the vertex
+Vertices* Graph:: findVertex(int ind) {
+    for (vector<Vertices*>::iterator it = allVert.begin(); it!= allVert.end(); it++) {
+        if ((*it)->index == ind) {
+            return (*it);
+        }
+    }
+    printf("Vertex not found.\n");
+    return NULL;
+}
+
+
+//Recursive depth first search to get the graph
+//algorithm is ismplified as we know that the graph is MST
+void Graph::DFS(int x, vector<int>* order) {
+    Vertices* v;
+    v = findVertex(x);
+    v -> visited = true;
+    order->push_back(x);
+    
+    for (vector<int>::iterator it = v->neighbors.begin();
+         it != v->neighbors.end(); it++) {
+        Vertices* w = findVertex(*it);
+        if (!w->visited) {
+            DFS(*it,order);
+        }
     }
 }
 
@@ -55,106 +81,56 @@ Graph::Graph(std::vector< pair<int, int> > initialsolution, std::vector<std::pai
 
 
 
+int main(){
+    vector< pair<int, int> > initialsolution;
+    vector<pair<int,int> > coordinates;
+    
+    initialsolution.push_back(pair<int,int>(0,2));
+    initialsolution.push_back(pair<int,int>(1,2));
+    initialsolution.push_back(pair<int,int>(3,2));
+    initialsolution.push_back(pair<int,int>(3,4));
+    initialsolution.push_back(pair<int,int>(4,5));
+    
+    coordinates.push_back(pair<int,int>(0,0)); //the #0 item
+    coordinates.push_back(pair<int,int>(1,0)); //the #1 item
+    coordinates.push_back(pair<int,int>(2,0)); //the #2 item
+    coordinates.push_back(pair<int,int>(4,3)); //the #3 item
+    coordinates.push_back(pair<int,int>(5,3)); //the #4 item
+    coordinates.push_back(pair<int,int>(10,10)); //the #5 item
 
-/*
- * The Vertices Class of the Graph
- *
- */
+    Graph* thisGraph = new Graph();
+    thisGraph->createGraph(initialsolution,coordinates);
+    thisGraph->printGraphContent();
 
-
-
-//constructor of vertices
-Vertices::Vertices(int x, int y, int i ){
-    xPos = x;
-    yPos = y;
-    weight= 0;   //default right now.
-    vertInd = i;
-    visited = false;
-    degree = 0;
-    //neighboars are null set
-}
-
-//destructor of vertices
-Vertices::~Vertices(){
-    delete[];
-}
-
-//function that returns the index
-int Vertices::GetIndex(){
-    return vertInd;
-};
-
-//function that retuns a set of neighbors
-std::set<int>  Vertices::GetNeighbors(){
-    return neighbors;
-}
-
-void Vertices::VisitVert(){
-    visited = true;
-}
-
-bool Vertices::GetVisit(){
-    return visited;
-}
-
-void addNeighbor(int n){
-    this.neighbors.push_back(n);
-}
-
-int getDegree(){
-    return degree;
-}
-
-void setDegree(int i){
-    degree = i;
-}
-
-
-/*
- * The Edge Class of the Graph
- *
- */
-
-
-//constructor of edge
-Edge::Edge(int index, int fromV , int toV, double weight2){
-    from = fromV;  //index of vertex that this edge is from
-    to = toV;      //index of vertex that this edge points to
-    weight= weight2; //the weight of the edge
-    edgeInd = index;
-    traversed = false;
-}
-
-//destructor of edge
-Edge::~Edge(){
-    delete[];
-}
-
-//function that returns the index
-int Edge::GetIndex(){
-    return edgeInd;
-};
-
-//function that retuns the weight
-double Edge::GetWeight(){
-    return weight;
-}
-
-//visit this edge
-void Edge::VisitEdge(){
-    traversed = true;
-}
-//function that returns whether this edge has been visited
-bool Edge::getTraversed(){
-    return traversed;
-}
-
-std::pair<int,int> getEnds() {
-    return pair<int,int>(fromV, toV);
+    
+    vector<int>* order;
+    order = new vector<int>();
+    thisGraph->DFS(0,order);
+    
+    for (vector<int>::iterator it=order->begin(); it!=order->end(); it++)
+    {
+        printf("%d\n",*it);
+    }
+    
+    delete thisGraph;
+    delete order;
+    
     
 }
 
 
 
+
+//This function prints out the graph content
+void Graph::printGraphContent(){
+    for (vector<Vertices*>::iterator it = allVert.begin(); it!= allVert.end(); it++) {
+        printf("Vertex %d, at (%d,%d)\n",(*it)->index, (*it)->xPos, (*it)->yPos);
+    }
+    for (vector<Edges*>::iterator it = allEdge.begin(); it!= allEdge.end(); it++) {
+        printf("Edge (%d,%d)\n",(*it)->fromIndex, (*it)->toIndex);
+    }
+    
+    printf("END\n");
+}
 
 
--- graph.h
+++ graph.h
@@ -2,43 +2,112 @@
 #include <stdlib.h>
 #include <fstream>
 #include <iostream>
-#include <set>
-#include <queue>
+#include <vector>
 #include <math.h>
 
 
 using namespace std;
 
 
-struct Vertices //the vertices in the graph
+typedef struct Vertices //the vertices in the graph
 {
     int index;
     int xPos;
     int yPos;
     bool visited;
-    vector <int> neighboars;
-};
+    vector <int> neighbors;
+}Vertices;
 
-struct Edges //the edges as given by the MST
+typedef struct Edges //the edges as given by the MST
 {
     int fromIndex;
     int toIndex;
-    double length;
+    //double length;
     bool revised;
-};
+}Edges;
 
 
 class Graph
 {
 public:
-    Graph(std::vector< pair<int, int> > initialsolution, std::vector<std::pair<int,int> > coordinates);
-    //constructor: construct graph from the file
-    
     ~Graph();
-private:
+    Graph(){
+        std::vector<Vertices*> allVert;
+        std::vector<Edges*> allEdge;
+    }
     
-    std::vector<Vertices> allVert;
-    std::vector<Edges> allEdge;
-    
+    void createGraph(std::vector< pair<int, int> > initialsolution, std::vector<std::pair<int,int> > coordinates);
+    bool isConnected(int,int);
+    void printGraphContent();
+    std::vector<Vertices*> allVert;
+    std::vector<Edges*> allEdge;
+    Vertices* findVertex(int ind);
+    void DFS(int, vector<int>*);
 };
+
+
+struct node{
+    int info;
+    struct node *next;
+};
+
+
+class stack{
+    struct node *top;
+public:
+    stack(); //constructor of stack
+    void push(int); //push a node index
+    int pop();
+    bool isEmpty();
+    void display();
+};
+
+stack::stack(){
+    top = NULL;
+}
+
+//push a new node onto the stack
+void stack::push(int data){
+    node *p;
+    p = new node;
+    p->info = data;
+    p->next = NULL;
+    if(top!=NULL){
+        p->next = top;
+    }
+    top = p;
+}
+
+//take a node out of the stack
+int stack::pop(){
+    struct node *temp;
+    int value;
+    if(top==NULL){
+        cout<<"\nThe stack is Empty"<<endl;
+    }else{
+        temp = top;
+        top = top->next;
+        value = temp->info;
+        delete temp;
+    }
+    return value;
+}
+
+bool stack::isEmpty(){
+    return (top == NULL);
+}
+
+void stack::display(){
+    struct node *p = top;
+    if(top==NULL){
+        cout<<"\nNothing to Display\n";
+    }else{
+        cout<<"\nThe contents of Stack\n";
+        while(p!=NULL){
+            cout<<p->info<<endl;
+            p = p->next;
+        }
+    }
+}
+
 
