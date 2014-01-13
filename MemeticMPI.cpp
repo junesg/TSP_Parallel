62968d50fa9d980c28d9f964d3a7729f3a57e93d
junesg <sgy8848@gmail.com>
2014-01-13 03:00:19 -0500

    getting close to parallel finish

--- MemeticMPI.cpp
+++ MemeticMPI.cpp
@@ -4,6 +4,8 @@
  *  Created by Yuan Shangguan
  *  Copyright 2013 
  *  University of Michigan Ann Arbor. All rights reserved.
+ *  method: 0: MST, 1:GA, 2:TwoOpt, 3:RayOpt, 4:KL, 5:StarOpt 
+ *	message coding: (double)convergence, (double)time, (double)lengthOfMethod, method array, method iteration array
  *
  */
 
@@ -44,7 +46,6 @@ int main(int argc, char** argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rankWorld);
     //get the size of the world, find out how many processors are executing at the same time
     MPI_Comm_size(MPI_COMM_WORLD, &sizeWorld);
-    
 
 	/* Initializing the solution in all of the processors*/
     string filename = "testDist.txt";
@@ -138,25 +139,98 @@ void singleRoundImprovement(doublylinkedlist* solutionDLL,
  * It then receives the resulting convergence rate from the slaves. 
  */
 static void master() {
-  	int sizeWorld;
+  	int sizeWorld, messageLength, source;
   	MPI_Status status;
   	double t_begin, t_end;
+   	double overallConvergence = 1;
+   	HashMap *historyOfCommands;
+   	LinkedHashEntry* nextRoundMethods; //this linkedHashEntry stores the methods for next rounds
+   	
    	
     t_begin = MPI_Wtime();
   	/* find out the number of processors in the common world communicator */
   	MPI_Comm_size(MPI_COMM_WORLD, &sizeWorld);
-	vector<int> MethodSequence;
-    vector<int> MethodIteration;
-
-  	
+  	historyOfCommands = new HashMap(sizeWorld); //the table size in the hashmap is fixed to the size of the world
   	/* Initialize the methods */
-  	/* method: 0: MST, 1:GA, 2:TwoOpt, 3:RayOpt, 4:KL, 5:StarOpt */
-    MethodSequence->push_back(rankWorld%6); //initialize a single method
-    //initialize the number of iterations for the initial method
-    MethodIteration->push_back(int ITERATION); 
-    if (rankWorld == proc_root) {
-        t_begin = MPI_Wtime();
-    }
+	int *MethodSequence;
+    int *MethodIteration;
+    double *incomingMessage;
+	
+    t_begin = MPI_Wtime();
+    
+  /* Loop over getting new work requests until there is no more work
+     to be done */
+  while (overallConvergence == 1) {
+		MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        MPI_Get_count(&status, MPI_DOUBLE, &messageLength);
+  		int receiveCount = 0;
+  		double convergence[sizeWorld];
+  		double timeTaken[sizeWorld];
+  		double index[sizeWorld];
+  		
+  		/* Loop through all results from slaves have been received */
+  		while(receiveCount < sizeWorld ){
+    	/* Receive results from a slave */
+    		incomingMessage = (double *)malloc(sizeof(double)*messageLength);
+    		MPI_Recv(&incomingMessage,           /* message buffer */
+             	messageLength,                 /* one data item */
+             	MPI_DOUBLE,        /* of type double real */
+             	MPI_ANY_SOURCE,    /* receive from any sender */
+             	MPI_ANY_TAG,       /* any type of message */
+             	MPI_COMM_WORLD,    /* default communicator */
+             	&status);          /* info about the received message */
+			source = status.Get_source();
+			convergence[source] = (*incomingMessage)[0];
+			timeTaken[source] = (*incomingMessage)[1];
+			index[source] = source;
+			historyOfCommands->put(source, incomingMessage);
+			receiveCount ++;
+		}
+		
+		quickSortProperties( &convergence, &timeTaken, &index);
+		cout<<"after sorting: "<<endl;
+/*for debugging */
+		for	(int i=0; i < sizeWorld; i++) {
+			cout<<"index: "<<(*index)[i]<<", conv: "<<(*convergence)[i]<<", time: "<<(*timeTaken)[i]<<endl;
+		}
+/*for debugging */
+		
+		/* Now process the results of this round, prepare for crossing of methods */
+		
+		
+		/* Send a new round : (double)NumberInMethod, method array, and send the method iteration array */
+		
+		
+		
+
+
+
+    /* Send the slave a new work unit */
+    MPI_Send(&work,             /* message buffer */
+             1,                 /* one data item */
+             MPI_INT,           /* data item is an integer */
+             status.MPI_SOURCE, /* to who we just received from */
+             WORKTAG,           /* user chosen message tag */
+             MPI_COMM_WORLD);   /* default communicator */
+    /* Get the next unit of work to be done */
+  }
+
+	t_end = MPI_Wtime();
+  /* 
+  //There's no more work to be done, so receive all the outstanding
+     results from the slaves. 
+  for (rank = 1; rank < ntasks; ++rank) {
+    MPI_Recv(&result, 1, MPI_DOUBLE, MPI_ANY_SOURCE,
+             MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+  }
+*/
+  	/* Tell all the slaves to exit by sending an empty message with the DIETAG. */
+  	for (rank = 1; rank < ntasks; ++rank) {
+    	MPI_Send(0, 0, MPI_INT, rank, DIETAG, MPI_COMM_WORLD);
+  	}
+  
+	cout<<"Time Spent: "<<t_end-t_begin<<endl;
+
   	
  //update method
     //update solution ? --> maybe only MST
@@ -166,77 +240,51 @@ static void master() {
     //repeat the loop till converge --> convergence criteria
 
     //spit out the best method and best solution from the 0 processor and the distance
-    
-    
-  
-
-
-
-
-
-
-  /* send one unit of work to each slave. */
-  for (rank = 1; rank < ntasks; ++rank) {
-//    /* Find the next item of work to do */
-//    work = get_next_work_item();
-
-    /* Send it to each rank */
-    MPI_Send(&work,             /* message buffer */
-             1,                 /* one data item */
-             MPI_INT,           /* data item is an integer */
-             rank,              /* destination process rank */
-             WORKTAG,           /* user chosen message tag */
-             MPI_COMM_WORLD);   /* default communicator */
-  }
-
-  /* Loop over getting new work requests until there is no more work
-     to be done */
-
-  work = get_next_work_item();
-  while (work != NULL) {
-
-    /* Receive results from a slave */
-
-    MPI_Recv(&result,           /* message buffer */
-             1,                 /* one data item */
-             MPI_DOUBLE,        /* of type double real */
-             MPI_ANY_SOURCE,    /* receive from any sender */
-             MPI_ANY_TAG,       /* any type of message */
-             MPI_COMM_WORLD,    /* default communicator */
-             &status);          /* info about the received message */
-
-    /* Send the slave a new work unit */
-
-    MPI_Send(&work,             /* message buffer */
-             1,                 /* one data item */
-             MPI_INT,           /* data item is an integer */
-             status.MPI_SOURCE, /* to who we just received from */
-             WORKTAG,           /* user chosen message tag */
-             MPI_COMM_WORLD);   /* default communicator */
-
-    /* Get the next unit of work to be done */
-
-    work = get_next_work_item();
-    
-
-  }
-
-  /* There's no more work to be done, so receive all the outstanding
-     results from the slaves. */
-
-  for (rank = 1; rank < ntasks; ++rank) {
-    MPI_Recv(&result, 1, MPI_DOUBLE, MPI_ANY_SOURCE,
-             MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-  }
-
-  	/* Tell all the slaves to exit by sending an empty message with the DIETAG. */
-  	for (rank = 1; rank < ntasks; ++rank) {
-    	MPI_Send(0, 0, MPI_INT, rank, DIETAG, MPI_COMM_WORLD);
-  	}
-  
-	cout<<"Time Spent: "<<t_end-t_begin<<endl;
-
+   
 }
+
+
+/* quickSort allows the */
+void quickSortProperties( double *convergence,  double *timeTaken,  double *index, int left, int left) {
+  int i = left, j = right;
+  double tmpConv, tmpTime,tmpInd;
+  
+  int pivot = arr[(left + right) / 2];
+
+  /* partition */
+  while (i <= j) {
+        while (conver_time_measure (convergence, timeTaken, i) < 
+        	conver_time_measure (convergence, timeTaken, pivot))
+              i++;
+        while (conver_time_measure (convergence, timeTaken, j)> 
+       		conver_time_measure (convergence, timeTaken, pivot))
+              j--;
+        if (i <= j) {
+			tmpInd = (*index)[i];
+			tmpConv = (*convergence)[i];
+			tmpTime = (*timeTaken)[i];
+			(*index)[i]=(*index)[j];
+			(*convergence)[i]=(*convergence)[j];
+			(*timeTaken)[i] = (*timeTaken)[j];
+			(*index)[j] = tmpInd;
+			(*convergence)[j] = tmpConv;
+			(*timeTaken)[j] = tmpTime;
+              i++;
+              j--;
+    }
+}
+/* recursion */
+if (left < j)
+    quickSort(convergence, timeTaken, left, j);
+if (i < right)
+        quickSort(convergence, timeTaken, i, right);
+}
+
+/* helper function to define the criteria for sorting */
+double conver_time_measure (double* converg, double* time, int pivot) {
+	return ((*converg)[pivot])*((*time)[pivot]);
+}
+
 
 
 static void 
@@ -260,7 +308,6 @@ slave(void)
     }
 
     /* Do the work */
-
         t1 = MPI_Wtime();
     /* start the loop of work */
 	for(int method=0; method < MethodSequence->size(); method++){
